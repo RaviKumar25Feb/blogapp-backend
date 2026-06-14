eb0f0696093eba244ba23b9fb09ac9304670c6cd
ravikumar25feb <RaviKumar25Feb@users.noreply.github.com>
2026-06-14 18:18:21 +0530

    fixed db connection

--- configs/dbConnect.js
+++ configs/dbConnect.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
+const dns = require("node:dns/promises");
 
 const dbConnect = async () => {
-    try{
-        await mongoose.connect(process.env.DATABASE_URI);
-        console.log(`DB connected sucessfully`);
-    } catch(error) {
-        console.log(error.message);
-        process.exit(1);
-    }
-}
+  try {
+    dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+    await mongoose.connect(process.env.DATABASE_URI);
+    console.log(`DB connected sucessfully`);
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = dbConnect;
